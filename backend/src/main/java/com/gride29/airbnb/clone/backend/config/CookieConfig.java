7a4574a2f5ff37dcce39ff5d6ef9b2c631002d8b
gride29 <cezario2000@gmail.com>
2023-05-30 19:45:17 +0200

    chore: remove unused imports

--- backend/src/main/java/com/gride29/airbnb/clone/backend/config/CookieConfig.java
+++ backend/src/main/java/com/gride29/airbnb/clone/backend/config/CookieConfig.java
@@ -1,12 +1,9 @@
 package com.gride29.airbnb.clone.backend.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.SessionCookieConfig;
 
