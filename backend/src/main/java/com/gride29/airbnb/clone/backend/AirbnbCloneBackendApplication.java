4c2042a950d808acfab17e0606a1177ddbd99f32
gride29 <cezario2000@gmail.com>
2023-06-20 15:56:56 +0200

    feat: update backend application name

--- backend/src/main/java/com/gride29/airbnb/clone/backend/AirbnbCloneBackendApplication.java
+++ backend/src/main/java/com/gride29/airbnb/clone/backend/AirbnbCloneBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityJwtMongodbApplication {
+public class AirbnbCloneBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityJwtMongodbApplication.class, args);
+        SpringApplication.run(AirbnbCloneBackendApplication.class, args);
     }
 
 }
